7a51f704e412b08390eaf4c069b263045e7ff41f
balram-kandoria <71412134+balram-kandoria@users.noreply.github.com>
2023-03-22 21:32:07 -0700

    Update Algorithm to produce Maze with no double thickness
    Bug exists with the MergingSet value

--- Kruskals algorithm.py
+++ Kruskals algorithm.py
@@ -1,4 +1,5 @@
 import random
+import numpy
 
 mazeWidth = 50
 mazeLength = 50
@@ -60,7 +61,7 @@ def output_image_Kruskals(filename, Puzzle, defaultWall):
     img.save(filename)
 
 
-def pickAWall(x, y, Puzzle, defaultWall):
+def pickAWall(x, y, Puzzle, defaultWall, removeZeros, RandUn):
     Direction = ['up', 'down', 'left', 'right']
     validPathChosen = False
     checkUP = False
@@ -69,65 +70,109 @@ def pickAWall(x, y, Puzzle, defaultWall):
     checkRT = False
     dir = 0
     noValid = False
-    print('Function While loop')
+    # print('Function While loop')
     while not validPathChosen:
         step = random.choice(Direction)
         if step == 'up':
             try:
                 # and findCycle(Puzzle, x, y+1) == False:
-                if (Puzzle[x][y+1][0] != Puzzle[x][y][0]):
-                    if Puzzle[x][y+1][0] != 1 and (((Puzzle[x][y+1][1] == defaultWall) and (Puzzle[x][y+1][3] == defaultWall)) or ((Puzzle[x][y+1][2] == defaultWall) and (Puzzle[x][y+1][4] == defaultWall))):
-                        y += 1
-                        validPathChosen = True
-                        dir = 1
+                if removeZeros:
+                    if (Puzzle[x][y+1][0] != Puzzle[x][y][0]):
+                        if Puzzle[x][y+1][0] != 1 and ((Puzzle[x][y+1][1] + Puzzle[x][y+1][2] + Puzzle[x][y+1][3] + Puzzle[x][y+1][4]) > -2) and ((Puzzle[x][y][1] + Puzzle[x][y][2] + Puzzle[x][y][3] + Puzzle[x][y][4]) > -2):
+                            y += 1
+                            validPathChosen = True
+                            dir = 1
+                        else:
+                            checkUP = True
                     else:
                         checkUP = True
                 else:
-                    checkUP = True
+                    if (Puzzle[x][y+1][0] != Puzzle[x][y][0]):
+                        if Puzzle[x][y+1][0] != 1 and ((Puzzle[x][y+1][1] + Puzzle[x][y+1][2] + Puzzle[x][y+1][3] + Puzzle[x][y+1][4]) > -2) and ((Puzzle[x][y][1] + Puzzle[x][y][2] + Puzzle[x][y][3] + Puzzle[x][y][4]) > -2):
+                            y += 1
+                            validPathChosen = True
+                            dir = 1
+                        else:
+                            checkUP = True
+                    else:
+                        checkUP = True
             except:
                 checkUP = True
 
         elif step == 'down':
             try:
                 # and findCycle(Puzzle, x, y-1) == False:
-                if (Puzzle[x][y-1][0] != Puzzle[x][y][0]):
-                    if Puzzle[x][y-1][0] != 1 and (((Puzzle[x][y-1][1] == defaultWall) and (Puzzle[x][y-1][3] == defaultWall)) or ((Puzzle[x][y-1][2] == defaultWall) and (Puzzle[x][y-1][4] == defaultWall))):
-                        y -= 1
-                        validPathChosen = True
-                        dir = 2
+                if removeZeros:
+                    if (Puzzle[x][y-1][0] != Puzzle[x][y][0]):
+                        if Puzzle[x][y-1][0] != 1 and ((Puzzle[x][y-1][1] + Puzzle[x][y-1][2] + Puzzle[x][y-1][3] + Puzzle[x][y-1][4]) > -2) and ((Puzzle[x][y][1] + Puzzle[x][y][2] + Puzzle[x][y][3] + Puzzle[x][y][4]) > -2):
+                            y -= 1
+                            validPathChosen = True
+                            dir = 2
+                        else:
+                            checkDN = True
                     else:
                         checkDN = True
                 else:
-                    checkDN = True
+                    if (Puzzle[x][y-1][0] != Puzzle[x][y][0]):
+                        if Puzzle[x][y-1][0] != 1 and ((Puzzle[x][y-1][1] + Puzzle[x][y-1][2] + Puzzle[x][y-1][3] + Puzzle[x][y-1][4]) > -2) and ((Puzzle[x][y][1] + Puzzle[x][y][2] + Puzzle[x][y][3] + Puzzle[x][y][4]) > -2):
+                            y -= 1
+                            validPathChosen = True
+                            dir = 2
+                        else:
+                            checkDN = True
+                    else:
+                        checkDN = True
             except:
                 checkDN = True
 
         elif step == 'left':
             try:
                 # and findCycle(Puzzle, x-1, y) == False:
-                if (Puzzle[x-1][y][0] != Puzzle[x][y][0]):
-                    if Puzzle[x-1][y][0] != 1 and (((Puzzle[x-1][y][1] == defaultWall) and (Puzzle[x-1][y][3] == defaultWall)) or ((Puzzle[x-1][y][2] == defaultWall) and (Puzzle[x-1][y][4] == defaultWall))):
-                        x -= 1
-                        validPathChosen = True
-                        dir = 3
+                if removeZeros:
+                    if (Puzzle[x-1][y][0] != Puzzle[x][y][0]):
+                        if Puzzle[x-1][y][0] != 1 and ((Puzzle[x-1][y][1] + Puzzle[x-1][y][2] + Puzzle[x-1][y][3] + Puzzle[x-1][y][4]) > -2) and ((Puzzle[x][y][1] + Puzzle[x][y][2] + Puzzle[x][y][3] + Puzzle[x][y][4]) > -2):
+                            x -= 1
+                            validPathChosen = True
+                            dir = 3
+                        else:
+                            checkLT = True
                     else:
                         checkLT = True
                 else:
-                    checkLT = True
+                    if (Puzzle[x-1][y][0] != Puzzle[x][y][0]):
+                        if Puzzle[x-1][y][0] != 1 and ((Puzzle[x-1][y][1] + Puzzle[x-1][y][2] + Puzzle[x-1][y][3] + Puzzle[x-1][y][4]) > -2) and ((Puzzle[x][y][1] + Puzzle[x][y][2] + Puzzle[x][y][3] + Puzzle[x][y][4]) > -2):
+                            x -= 1
+                            validPathChosen = True
+                            dir = 3
+                        else:
+                            checkLT = True
+                    else:
+                        checkLT = True
             except:
                 checkLT = True
         elif step == 'right':
             try:
                 # and findCycle(Puzzle, x+1, y) == False:
-                if (Puzzle[x+1][y] != Puzzle[x][y]):
-                    if Puzzle[x+1][y][0] != 1 and (((Puzzle[x+1][y][1] == defaultWall) and (Puzzle[x+1][y][3] == defaultWall)) or ((Puzzle[x+1][y][2] == defaultWall) and (Puzzle[x+1][y][4] == defaultWall))):
-                        x += 1
-                        validPathChosen = True
-                        dir = 4
+                if removeZeros:
+                    if (Puzzle[x+1][y] != Puzzle[x][y]):
+                        if Puzzle[x+1][y][0] != 1 and ((Puzzle[x+1][y][1] + Puzzle[x+1][y][2] + Puzzle[x+1][y][3] + Puzzle[x+1][y][4]) > -2) and ((Puzzle[x][y][1] + Puzzle[x][y][2] + Puzzle[x][y][3] + Puzzle[x][y][4]) > -2):
+                            x += 1
+                            validPathChosen = True
+                            dir = 4
+                        else:
+                            checkRT = True
                     else:
                         checkRT = True
                 else:
-                    checkRT = True
+                    if (Puzzle[x+1][y] != Puzzle[x][y]) and Puzzle[x][y] == RandUn:
+                        if Puzzle[x+1][y][0] != 1 and ((Puzzle[x+1][y][1] + Puzzle[x+1][y][2] + Puzzle[x+1][y][3] + Puzzle[x+1][y][4]) > -2) and ((Puzzle[x][y][1] + Puzzle[x][y][2] + Puzzle[x][y][3] + Puzzle[x][y][4]) > -2):
+                            x += 1
+                            validPathChosen = True
+                            dir = 4
+                        else:
+                            checkRT = True
+                    else:
+                        checkRT = True
             except:
                 checkRT = True
 
@@ -172,17 +217,20 @@ while not startChosen:
 
 uniqueValue = 4
 removeZeros = True
-for run in range(5000):
+MergingSet = -1
+UnqiueValSet = []
+visitedSet = []
+for run in range(4000):
     count = 0
     # Choose a random starting point
     if run != 0:
         startChosen = False
-        print('Main While loop')
+        # print('Main While loop')
         temp = []
         while not startChosen:
             if removeZeros:
-                for i in range(mazeLength):
-                    for j in range(mazeWidth):
+                for i in range(mazeWidth):
+                    for j in range(mazeLength):
                         if Puzzle[i][j][0] == 0:
                             temp.append([i, j])
 
@@ -192,10 +240,27 @@ for run in range(5000):
                     rnd_indxY = choice[1]
                 else:
                     removeZeros = False
+                    UnqiueValSet = []
+                    for width in range(mazeWidth):
+                        for length in range(mazeLength):
+                            if Puzzle[width][length][0] > 1:
+                                UnqiueValSet.append([width, length])
 
             elif not removeZeros:
-                rnd_indxX = random.randrange(len(Puzzle))
-                rnd_indxY = random.randrange(len(Puzzle[0]))
+                try:
+                    choice = random.randrange(len(UnqiueValSet))
+                    rnd_indxX = UnqiueValSet[choice][0]
+                    rnd_indxY = UnqiueValSet[choice][1]
+                    UnqiueValSet.pop(choice)
+                    MergingSet = Puzzle[rnd_indxX][rnd_indxY][0]
+                    print(MergingSet)
+                    if len(UnqiueValSet) == 0:
+                        for width in range(mazeWidth):
+                            for length in range(mazeLength):
+                                if Puzzle[width][length][0] > 1:
+                                    UnqiueValSet.append([width, length])
+                except:
+                    continue
 
             if Puzzle[rnd_indxX][rnd_indxY][0] != 1:
                 if Puzzle[rnd_indxX][rnd_indxY][0] == 0:
@@ -207,7 +272,8 @@ for run in range(5000):
 
     xold = rnd_indxX
     yold = rnd_indxY
-    x, y, dir, noValid = pickAWall(rnd_indxX, rnd_indxY, Puzzle, defaultWall)
+    x, y, dir, noValid = pickAWall(
+        rnd_indxX, rnd_indxY, Puzzle, defaultWall, removeZeros, MergingSet)
     # print(x)
     # print(y)
     # print(dir)
@@ -222,28 +288,36 @@ for run in range(5000):
         # if Puzzle[x][y][0] > 0:
         #     print('Value needs to be replaced')
 
+         # Create a list of the set identifiers for merging
+        if not removeZeros:
+            assignSet = MergingSet
+
         Puzzle[x][y][0] = assignSet
+
         middleNode = -1
         if dir == 1:
             # middleNode = Puzzle[x][y-1][0]
             # Puzzle[x][y-1][0] = assignSet
             # Puzzle[x][y-1][2] = noWallValue
-            Puzzle[x][y][dir] = noWallValue
+            Puzzle[x][y][dir] = noWallValue/2
+            Puzzle[xold][yold][2] = noWallValue/2
         elif dir == 2:
             # middleNode = Puzzle[x][y+1][0]
             # Puzzle[x][y+1][0] = assignSet
             # Puzzle[x][y+1][1] = noWallValue
-            Puzzle[x][y][dir] = noWallValue
-
+            Puzzle[x][y][dir] = noWallValue/2
+            Puzzle[xold][yold][1] = noWallValue/2
         elif dir == 3:
             # middleNode = Puzzle[x+1][y][0]
             # Puzzle[x+1][y][0] = assignSet
-            Puzzle[x][y][4] = noWallValue
+            Puzzle[x][y][4] = noWallValue/2
+            Puzzle[xold][yold][dir] = noWallValue/2
             # Puzzle[x+1][y][4] = noWallValue
         elif dir == 4:
             # middleNode = Puzzle[x-1][y][0]
             # Puzzle[x-1][y][0] = assignSet
-            Puzzle[x][y][3] = noWallValue
+            Puzzle[x][y][3] = noWallValue/2
+            Puzzle[xold][yold][dir] = noWallValue/2
             # Puzzle[x-1][y][3] = noWallValue
 
         uniqueValue += 1
@@ -283,6 +357,9 @@ for i in range(mazeWidth):
         temp.append(Puzzle[i][j][0])
     print()
 print(len(set(temp))-2)
+
+print(run)
+
 
 # for i in range(mazeWidth*mazeLength):
 #     print(Wallset[i],  end=" ")
